14499c1e8c8ab638e3a636f9901b48cb0145d67f
CharlieLei <lei_cheng2008@126.com>
2020-10-28 19:02:52 +0800

    完成树

--- nodelinkTree/main.js
+++ nodelinkTree/main.js
@@ -1,48 +1,106 @@
-let width = 600
-let height = 400
+const width = 860
+const height = 400
 
-let canvas = d3.select('body')
+const canvas = d3.select('body')
   .append('svg')
-  .attr('width', width)
-  .attr('height', height)
+  .attr('width', 960)
+  .attr('height', 960)
 
-let treemap = d3.treemap()
-  .tile(d3.treemapResquarify)
-  .size([width, height])
-  .round(true)
-  .paddingInner(10)
+const diagonal = (p, n) => {
+  return 'M' + n.y + ',' + n.x
+    + 'C' + n.y + ',' + (n.x + p.x) / 2
+    + ' ' + p.y + ',' + (n.x + p.x) / 2
+    + ' ' + p.y + ',' + p.x
+}
+
+let dataRoot = {}
 
 d3.json('../data/tree.json').then(data => {
-  // var tree = d3.tree()
-  //   .size([height, width - 160])
-  // var nodes = tree.nodes(data),
-  //   links = tree.links(nodes)
   // 将数据转成一棵树
-  const root = treeify(data)
-  console.log(root)
-  const res = TreeLayout(root)
-  console.log(res)
-  // let cells = canvas.selectAll("g")
-  //   .data(root.leaves())
-  //   .enter()
-  //   .append("g")
-  //   .attr("transform", d => { return "translate(" + d.x0 + "," + d.y0 + ")" })
-  //
-  // cells.append("rect")
-  //   .attr("width", d => { return d.x1 - d.x0 })
-  //   .attr("height", d => { return d.y1 - d.y0 })
-
+  let root = treeify(data)
+  dataRoot = TreeLayout(root, 20, 120)
+  update(dataRoot)
 })
+
+function update (root) {
+  let nodes = listify(root)
+
+  let node = canvas.selectAll('g.node')
+    .data(nodes)
+
+  // ****************** 结点 ***************************
+  let nodeEnter = node.enter()
+    .append('g')
+    .attr('class', 'node')
+    .attr('transform', d => { return 'translate(' + (50 + d.y) + ',' + (450 + d.x) + ')' })
+    .on('click', click)
+  nodeEnter.append('circle')
+    .attr('r', 4.5)
+    .attr('fill', d => {
+      return d.hiddenChildren ? 'blue' : 'lightsteelblue'
+    })
+  nodeEnter.append('text')
+    .attr('dy', '0.35em')
+    .attr('dx', '0.5em')
+    .attr('text-anchor', 'start')
+    .text(d => {return d.data.name})
+
+  // let nodeUpdate = nodeEnter.merge(node)
+  //   .attr('fill', '#fff')
+  //   .attr('stroke', 'steelblue')
+  //   .attr('stroke-width', '3px;')
+  //   .style('font', '12px sans-serif')
+  // nodeUpdate.transition()
+  //   .duration(750)
+  //   .attr('transform', function (event, i, arr) {
+  //     const d = d3.select(this).datum()
+  //     return 'translate(' + (50 + d.y) + ',' + (450 + d.x) + ')'
+  //   })
+  //
+  // let nodeExit = node.exit()
+  //   .transition()
+  //   .duration(750)
+  //   .attr("transform", function(event, i, arr) {
+  //     const d = d3.select(this).datum();
+  //     return "translate(" + (50 + source.y) + "," + ( 450 + source.x) + ")";
+  //   })
+  //   .remove();
+  // nodeExit.select('circle')
+  //   .attr('r', 1e-6);
+  // nodeExit.select('text')
+  //   .style('fill-opacity', 1e-6)
+
+  let link = canvas.selectAll('path.node')
+    .data(nodes)
+
+  let linkEnter = link.enter()
+    .append('path')
+    .attr('transform', d => { return 'translate(' + 50 + ',' + 450 + ')' })
+    .attr('d', d => {
+      return d.parent ? diagonal(d.parent, d) : ''
+    })
+    .attr('fill', 'none')
+    .attr('stroke', '#ccc')
+    .attr('stroke-width', '2px')
+
+}
+
+function click (event, d) {
+  d.hiddenChildren = !d.hiddenChildren
+  update(dataRoot)
+}
 
 const distance = 1
 
-function TreeLayout (root) {
+function TreeLayout (root, x, y, dx, dy) {
   // 初始化结点
   InitNodes(root)
 
   FirstWalk(root)
   // 根据mod值计算最终的x值
   SecondWalk(root, -root.prelim)
+  // 根据大小对树进行放缩
+  ScaleTree(root, x, y, dx, dy)
 
   return root
 }
@@ -62,17 +120,19 @@ function InitNodes (root) {
 }
 
 function FirstWalk (v) {
+  let w = GetPrevSibling(v)
   if (IsLeaf(v)) {
-    v.prelim = 0
+    if (w === null) v.prelim = 0
+    else v.prelim = w.prelim + distance
   } else {
     let defaultAncestor = GetLeftmostChild(v)
-    v.children.forEach((w, i) => {
+    for (let i = 0; i < v.children.length; i++) {
+      let w = v.children[i]
       FirstWalk(w)
       defaultAncestor = Apportion(w, defaultAncestor)
-    })
+    }
     ExecuteShifts(v)
     let midpoint = (GetLeftmostChild(v).prelim + GetRightmostChild(v).prelim) / 2
-    let w = GetPrevSibling(v)
     if (w !== null) {
       v.prelim = w.prelim + distance
       v.mod = v.prelim - midpoint
@@ -143,7 +203,8 @@ function MoveSubtree (wn, wp, shift) {
 function ExecuteShifts (v) {
   let shift = 0
   let change = 0
-  for (let i = 0; i < v.children.length; i++) {
+  // 是从右到左遍历子结点
+  for (let i = v.children.length - 1; i >= 0; i--) {
     let w = v.children[i]
     w.prelim += shift
     w.mod += shift
@@ -160,9 +221,39 @@ function Ancestor (vin, v, defaultAncestor) {
 function SecondWalk (v, m) {
   v.x = v.prelim + m
   v.y = v.depth
-  v.children.forEach((w, i) => {
+  for (let i = 0; i < v.children.length; i++) {
+    let w = v.children[i]
     SecondWalk(w, m + v.mod)
+  }
+}
+
+function ScaleTree (root, dx, dy) {
+  let nodes = listify(root)
+  nodes.forEach((node) => {
+    node.x *= dx
+    node.y = node.depth * dy
   })
+  // if (nodeSize) {
+  //   nodes.forEach((node) => {
+  //     node.x *= nodeSize[0];
+  //     node.y = node.depth * nodeSize[1];
+  //   })
+  // } else {
+  //   let leftmost = root, rightmost = root, bottommost = root
+  //   nodes.forEach((node) => {
+  //     if (node.x < leftmost.x) leftmost = node
+  //     if (node.x > rightmost.x) rightmost = node
+  //     if (node.depth > bottommost.depth) bottommost = node
+  //   })
+  //
+  //   let tx = distance - leftmost.x // 给左边留出空间，让最左边的点的x为1
+  //   let kx = dx / (rightmost.x - leftmost.x + 2 * distance) // 给两边留出空间
+  //   let ky = dy / (bottommost.y - root.y + 2 * distance)
+  //   nodes.forEach((node) => {
+  //     node.x0 = (node.x + tx) * kx
+  //     node.y0 = (node.y) * ky
+  //   })
+  // }
 }
 
 function TreeNode (data) {
@@ -178,6 +269,8 @@ function TreeNode (data) {
   this.shift = 0
   this.thread = null
   this.number = -1
+
+  this.hiddenChildren = false
 }
 
 function treeify (data) {
@@ -202,6 +295,22 @@ function treeify (data) {
   return root
 }
 
+function listify (root) {
+  let res = []
+  let node, nodeQueue = [root]
+  while (node = nodeQueue.pop()) {
+    res.push(node)
+    if (!node.hiddenChildren) {
+      let n = node.children.length
+      for (let i = 0; i < n; i++) {
+        let child = node.children[i]
+        nodeQueue.push(child)
+      }
+    }
+  }
+  return res
+}
+
 function IsLeaf (node) {
   return node.children.length === 0
 }
@@ -223,11 +332,6 @@ function IsSibling (node, sibling) {
 function GetPrevSibling (node) {
   if (node.parent === null || IsLeftMost(node)) return null
   else return node.parent.children[node.parent.children.indexOf(node) - 1]
-}
-
-function GetNextSibling (node) {
-  if (node.parent === null || IsRightMost(node)) return null
-  else return node.parent.children[node.parent.children.indexOf(node) + 1]
 }
 
 function GetLeftmostSibling (node) {
